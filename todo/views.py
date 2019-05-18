d2c6ffa19c2234351cd8ee5b8ca0c54ca4341499
shunkichis <shunkichi.sato@gmail.com>
2019-05-18 22:41:25 +0900

    viewsの各関数をクラスベースにしました

--- todo/views.py
+++ todo/views.py
@@ -4,21 +4,54 @@ from django.urls import reverse
 from .models import Post
 from .forms import PostForm
 
-
-def index(req):
-    posts = Post.objects.all()
-    form = PostForm()
-    context = {'posts': posts, 'form': form, }
-    return render(req, 'todo/index.html', context)
+from django.views import View
+from django.views.generic.edit import CreateView, DeleteView
 
 
-def add(req):
-    form = PostForm(req.POST)
-    form.save(commit=True)
-    return HttpResponseRedirect(reverse('index'))
+# def index(req):
+#     posts = Post.objects.all()
+#     form = PostForm()
+#     context = {'posts': posts, 'form': form, }
+#     return render(req, 'todo/index.html', context)
+#
+#
+# def add(req):
+#     form = PostForm(req.POST)
+#     form.save(commit=True)
+#     return HttpResponseRedirect(reverse('index'))
+#
+#
+# def delete(req, id=None):
+#     post = get_object_or_404(Post, pk=id)
+#     post.delete()
+#     return HttpResponseRedirect(reverse('index'))
+
+class PreviewTodo(View):
+    def get(self, req, *args, **kwargs):
+        posts = Post.objects.all()
+        form = PostForm
+        context = {'posts': posts, 'form': form}
+        return render(req, 'todo/index.html', context)
 
 
-def delete(req, id=None):
-    post = get_object_or_404(Post, pk=id)
-    post.delete()
-    return HttpResponseRedirect(reverse('index'))
+index = PreviewTodo.as_view()
+
+
+class CreateTodo(CreateView):
+    def post(self, req, *args, **kwargs):
+        form = PostForm(req.POST)
+        form.save(commit=True)
+        return HttpResponseRedirect(reverse('index'))
+
+
+add = CreateTodo.as_view()
+
+
+class DeleteTodo(DeleteView):
+    def delete(self, req, id=None):
+        post = get_object_or_404(Post, pk=id)
+        post.delete()
+        return HttpResponseRedirect(reverse('index'))
+
+
+delete = DeleteTodo.as_view()
