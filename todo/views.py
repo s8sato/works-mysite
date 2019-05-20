62ca06d65e2c125ad6a4e77f020ed0e25678df84
satu-n <49983831+satu-n@users.noreply.github.com>
2019-05-21 00:30:07 +0900

    deadlineの他にもattrをまとめてpaeseするようにした

--- todo/views.py
+++ todo/views.py
@@ -8,24 +8,6 @@ from django.views.generic.edit import CreateView, DeleteView
 
 from .sprig import Line, Sprig
 
-# def index(req):
-#     posts = Post.objects.all()
-#     form = PostForm()
-#     context = {'posts': posts, 'form': form, }
-#     return render(req, 'todo/index.html', context)
-#
-#
-# def add(req):
-#     form = PostForm(req.POST)
-#     form.save(commit=True)
-#     return HttpResponseRedirect(reverse('index'))
-#
-#
-# def delete(req, id=None):
-#     post = get_object_or_404(Post, pk=id)
-#     post.delete()
-#     return HttpResponseRedirect(reverse('index'))
-
 
 class ShowTask(View):
     """"""
