b1216367cafdea130315bf5a66d5b9cffb2f7bf9
satu-n <49983831+satu-n@users.noreply.github.com>
2019-05-20 16:21:31 +0900

    うまくいかない * update_or_createのdefault={}のなかに書こうとするforeignkeyは instanceであることとstring的であることの両方が要求される * Stepのpk, loc, linkerは役割が被っているんじゃないか

--- todo/admin.py
+++ todo/admin.py
@@ -1,11 +1,26 @@
 from django.contrib import admin
-from .models import Task
+from .models import Step, Task
 
 
 # レコード表示機能
+class StepAdmin(admin.ModelAdmin):
+    list_display = ('loc', 'linker')
+    # list_display_links = ()
+
+# レコード表示機能
 class TaskAdmin(admin.ModelAdmin):
-    list_display = ('title', 'deadline')
-    # list_display_links = ('message', 'created_at')
+    list_display = ('title',
+                    'start',
+                    'expected_time',
+                    'actual_time',
+                    'deadline',
+                    'client',
+                    'is_done',
+                    'note',
+                    'initial_step',
+                    'terminal_step',)
+    # list_display_links = ()
 
 
+admin.site.register(Step, StepAdmin)
 admin.site.register(Task, TaskAdmin)
--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse,HttpResponseRedirect
 from django.urls import reverse
-from .models import Task
+from .models import Step, Task
 
 from django.views import View
 from django.views.generic.edit import CreateView, DeleteView
@@ -26,8 +26,10 @@ from .sprig import Line, Sprig
 #     post.delete()
 #     return HttpResponseRedirect(reverse('index'))
 
+
 class ShowTask(View):
-    def show(self, req, *args, **kwargs):
+    """"""
+    def get(self, req, *args, **kwargs):
         tasks = Task.objects.all()
         context = {'tasks': tasks}
         return render(req, 'todo/index.html', context)
@@ -37,18 +39,70 @@ index = ShowTask.as_view()
 
 
 class CreateTask(CreateView):
-    def create(self, req, *args, **kwargs):
-        sprig = Sprig(req.POST['sprig'])
-        for line in sprig.lines:
-            task = line.to_task(Task())
-            task.save(commit=True)
+    """"""
+    def post(self, req, *args, **kwargs):
+        try:
+            existing = Task.objects.all().order_by('-pk')[0].pk
+        except IndexError:
+            existing = 0
+        sprig = Sprig(req.POST['sprig'], existing)
+        for node in sprig.ad.nodes:
+            # self.step_as(node, sprig.ad.nodes).save()
+            Step.objects.update_or_create(
+                linker=sprig.ad.nodes[node]['linker'],
+                defaults={
+                    'loc': node,
+                    'linker': sprig.ad.nodes[node]['linker'],
+                }
+            )
+        for edge in sprig.ad.edges:
+            # self.task_as(edge, sprig.ad.edges).save()
+            Task.objects.update_or_create(
+                initial_step=Step.objects.get(loc=edge[0]),
+                terminal_step=Step.objects.get(loc=edge[1]),
+                defaults={
+                    'title': sprig.ad.edges[edge]['title'],
+                    'start': sprig.ad.edges[edge]['start'],
+                    'expected_time': sprig.ad.edges[edge]['expected_time'],
+                    'actual_time': sprig.ad.edges[edge]['actual_time'],
+                    'deadline': sprig.ad.edges[edge]['deadline'],
+                    'client': sprig.ad.edges[edge]['client'],
+                    'is_done': sprig.ad.edges[edge]['is_done'],
+                    'note': sprig.ad.edges[edge]['note'],
+                    'initial_step': Step.objects.get(loc=edge[0]),
+                    'terminal_step': Step.objects.get(loc=edge[1]),
+                }
+            )
         return HttpResponseRedirect(reverse('index'))
+
+    # @staticmethod
+    # def step_as(node, nodes):
+    #     step = Step()
+    #     step.loc = node
+    #     step.linker = nodes[node]['linker']
+    #     return step
+    #
+    # @staticmethod
+    # def task_as(edge, edges):
+    #     task = Task()
+    #     task.title = edges[edge]['title']
+    #     task.start = edges[edge]['start']
+    #     task.expected_time = edges[edge]['expected_time']
+    #     task.actual_time = edges[edge]['actual_time']
+    #     task.deadline = edges[edge]['deadline']
+    #     task.client = edges[edge]['client']
+    #     task.is_done = edges[edge]['is_done']
+    #     task.note = edges[edge]['note']
+    #     task.initial_step = Step.objects.get(loc=edge[0])
+    #     task.terminal_step = Step.objects.get(loc=edge[1])
+    #     return task
 
 
 add = CreateTask.as_view()
 
 
 class DeleteTask(DeleteView):
+    """"""
     def delete(self, req, id=None):
         task = get_object_or_404(Task, pk=id)
         task.delete()
