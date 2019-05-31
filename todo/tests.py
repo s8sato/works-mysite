8ea800108c4c8c1009faf15ecd4e2c01b0b9bbaf
satu-n <49983831+satu-n@users.noreply.github.com>
2019-05-31 10:12:58 +0900

    ディレクトリ移動

--- todo/tests.py
+++ todo/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+import datetime
+
+ze = datetime.datetime(2019, 6, 26)
+
+fo  = ze + datetime.timedelta(weeks=20)
+
+ei  = fo + datetime.timedelta(weeks=20)
+
+pass
